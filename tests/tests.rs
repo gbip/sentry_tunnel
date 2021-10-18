ee2296a4e5d5f66f59cb52cec3c0eada52e65a27
Paul Florence <paul.florence@makina-corpus.com>
2021-10-18 14:47:34 +0200

    Test for invalid project ids

--- tests/tests.rs
+++ tests/tests.rs
@@ -6,6 +6,7 @@ mod tests {
     use mime::Mime;
     use sentry_tunnel::config::Config;
     use sentry_tunnel::server::router;
+    use sentry_tunnel::tunnel::BodyError;
 
     #[test]
     fn test_correct_behaviour() {
@@ -40,5 +41,44 @@ mod tests {
             .unwrap();
 
         assert_eq!(response.status(), StatusCode::OK);
+    }
+
+    #[test]
+    fn test_invalid_project_id() {
+        let test_config = Config {
+            remote_host: "https://sentry.example.com/".to_string(),
+            project_ids: vec!["5".to_string()],
+            port: 7878,
+            tunnel_path: "/tunnel".to_string(),
+            ip: "0.0.0.0".to_string(),
+        };
+        let test_server = TestServer::new(router(
+            &test_config.tunnel_path.clone(),
+            test_config.clone(),
+        ))
+        .unwrap();
+        let json = r#"{"sent_at":"2021-10-14T17:10:40.136Z","sdk":{"name":"sentry.javascript.browser","version":"6.13.3"},"dsn":"https://public@sentry.example.com/4"}
+        {"type":"session"}
+        {"sid":"751d80dc94e34cd282a2cf1fe698a8d2","init":true,"started":"2021-10-14T17:10:40.135Z","timestamp":"2021-10-14T17:10:40.135Z","status":"ok","errors":0,"attrs":{"release":"test_project@1.0"}"#;
+        let mime = "application/json".parse::<Mime>().unwrap();
+        let response = test_server
+            .client()
+            .post(
+                "http://localhost".to_owned() + &test_config.tunnel_path,
+                json,
+                mime,
+            )
+            .with_header(
+                header::CONTENT_LENGTH,
+                HeaderValue::from_str(&format!("{}", json.as_bytes().len())).unwrap(),
+            )
+            .perform()
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+        let body = response.read_body().unwrap();
+        let expc = format!("{}", BodyError::InvalidProjectId);
+
+        assert_eq!(&body[..], expc.as_bytes());
     }
 }
